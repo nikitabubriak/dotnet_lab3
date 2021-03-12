34e514481651ea5f5a100d3bede1c274a7b3a392
Nikita Bubriak <31805409+nikitabubriak@users.noreply.github.com>
2021-03-12 14:39:51 +0200

    setup

---
+++ lab3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ * Lab 3
+ * Variant 15
+ * 
+ * Реалізувати ігровий простір, 
+ * що подає персонажа з наступними необхідними атрибутами 
+ * (позиція персонажу, склад артефактів, рівень "здоров'я" тощо). 
+ * Реалізувати механізм збереження/встановлення стану персонажа.
+ */
+
+/*
+ * Memento is a behavioral design pattern 
+ * which makes it possible to save and restore 
+ * the previous state of an object without revealing the details of its implementation.
+ */
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+
+
+        }
+
+    }
+
+
+}
